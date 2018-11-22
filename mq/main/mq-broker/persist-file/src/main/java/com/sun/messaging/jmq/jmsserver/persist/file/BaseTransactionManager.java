4143b59f5a4b9b7054bb5f8bd2fff888a66116ab
Piotrek Zygielo <piotr@zygielo.pl>
2018-11-22 15:12:56 +0100

    Bring dead code back to life
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- mq/main/mq-broker/persist-file/src/main/java/com/sun/messaging/jmq/jmsserver/persist/file/BaseTransactionManager.java
+++ mq/main/mq-broker/persist-file/src/main/java/com/sun/messaging/jmq/jmsserver/persist/file/BaseTransactionManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -181,8 +181,8 @@ public abstract class BaseTransactionManager {
             logger.log(Logger.DEBUG, msg);
         }
         synchronized (playingToMessageStore) {
-            Object found = playingToMessageStore.remove(tid);
-            if (found == null) {
+            boolean found = playingToMessageStore.remove(tid);
+            if (!found) {
                 String msg = getPrefix() + " playingToMessageStoreComplete(): could not find " + tid;
                 logger.log(Logger.WARNING, msg);
             }
