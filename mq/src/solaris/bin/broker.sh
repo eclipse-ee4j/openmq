dce246d9f646230eeff1eff079ec085eb42832b0
Piotrek Zygielo <piotr@zygielo.pl>
2018-11-23 14:52:39 +0100

    javax.transaction-api.jar -> jakarta.transaction-api.jar
    Fixes #456
    
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- mq/src/solaris/bin/broker.sh
+++ mq/src/solaris/bin/broker.sh
@@ -89,7 +89,7 @@ fi
 
 jvm_args="$def_jvm_args $jvm_args -Dimq.home=$imq_home"
 
-_classes=$dependlibs/javax.jms-api.jar:$imq_home/../../share/opt/classes:$dependlibs/grizzly-framework.jar:$dependlibs/grizzly-portunif.jar:$dependlibs/glassfish-api.jar:$dependlibs/hk2-api.jar:$dependlibs/grizzly-http.jar:$dependlibs/grizzly-http-server.jar:$dependlibs/grizzly-http-servlet.jar:$dependlibs/jakarta.servlet-api.jar:$dependlibs/grizzly-websockets.jar:$dependlibs/javax.json.jar:$dependlibs/javax.transaction-api.jar:$dependlibs/jhall.jar:$dependlibs/fscontext.jar:$dependlibs/audit.jar:$dependlibs/bdb_je.jar
+_classes=$dependlibs/javax.jms-api.jar:$imq_home/../../share/opt/classes:$dependlibs/grizzly-framework.jar:$dependlibs/grizzly-portunif.jar:$dependlibs/glassfish-api.jar:$dependlibs/hk2-api.jar:$dependlibs/grizzly-http.jar:$dependlibs/grizzly-http-server.jar:$dependlibs/grizzly-http-servlet.jar:$dependlibs/jakarta.servlet-api.jar:$dependlibs/grizzly-websockets.jar:$dependlibs/javax.json.jar:$dependlibs/jakarta.transaction-api.jar:$dependlibs/jhall.jar:$dependlibs/fscontext.jar:$dependlibs/audit.jar:$dependlibs/bdb_je.jar
 
 # Additional classes possibly needed for JDBC provider
 _classes=$_classes:$imq_home/lib/ext
