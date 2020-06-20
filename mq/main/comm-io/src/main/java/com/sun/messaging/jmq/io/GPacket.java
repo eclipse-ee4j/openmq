6f3920cc238fa1544f98799c994309ca748fd653
Piotrek Zygielo <piotr@zygielo.pl>
2020-06-20 10:46:52 +0200

    Remove condition that is always true
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- mq/main/comm-io/src/main/java/com/sun/messaging/jmq/io/GPacket.java
+++ mq/main/comm-io/src/main/java/com/sun/messaging/jmq/io/GPacket.java
@@ -723,7 +723,7 @@ public class GPacket {
                 count = in.read(b, off + n, len - n);
             } catch (InterruptedIOException e) {
                 // if we really have read nothing .. throw an ex
-                if (!retry && n == 0 && count == 0 && e.bytesTransferred == 0) {
+                if (!retry && n == 0 && e.bytesTransferred == 0) {
                     throw new InterruptedIOException("no data available");
                 }
 
