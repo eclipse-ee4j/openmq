ef72064a2494e317966915e8d947836475dcc550
Piotrek Zygielo <piotr@zygielo.pl>
2020-08-01 11:29:18 +0200

    Remove unused local variable
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- mq/main/logger/src/main/java/com/sun/messaging/jmq/util/log/SysLogHandler.java
+++ mq/main/logger/src/main/java/com/sun/messaging/jmq/util/log/SysLogHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -140,7 +140,6 @@ public class SysLogHandler extends Handler {
     public void configure(String facilityStr, String logpidStr, String logconsoleStr, String identityStr, String outputStr)
             throws IllegalArgumentException, UnsatisfiedLinkError {
 
-        String error_msg = null;
         // LogManager lm = LogManager.getLogManager();
 
         synchronized (SysLogHandler.class) {
@@ -151,7 +150,6 @@ public class SysLogHandler extends Handler {
                     setFacility(SysLog.LOG_DAEMON);
                 } catch (Exception e) {
                     // error_msg = rb.getString(rb.W_BAD_NFORMAT, property, value);
-                    error_msg = "Bad syslog facility " + PREFIX + FACILITY + "=" + facilityStr;
                 }
             }
 
@@ -176,7 +174,6 @@ public class SysLogHandler extends Handler {
                     Level levelSetByConfig = Logger.levelIntToJULLevel(configLevel);
                     this.setLevel(levelSetByConfig);
                 } catch (IllegalArgumentException e) {
-                    error_msg = (error_msg != null ? error_msg + "\n" : "") + PREFIX + OUTPUT + ": " + e.getMessage();
                 }
             }
 
