b4957b29c6da102eba22996865639f2584281234
Piotrek Zygielo <piotr@zygielo.pl>
2020-09-15 21:51:37 +0200

    Deprecate FeatureUnavailableException class
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- mq/main/mq-broker/broker-comm/src/main/java/com/sun/messaging/jmq/jmsserver/util/FeatureUnavailableException.java
+++ mq/main/mq-broker/broker-comm/src/main/java/com/sun/messaging/jmq/jmsserver/util/FeatureUnavailableException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -25,8 +25,9 @@ import com.sun.messaging.jmq.jmsserver.comm.CommGlobals;
 
 /**
  * this is the sub-class for exceptions thrown by the broker
+ * @deprecated As of release 6. Will be removed without replacement in future release.
  */
-
+@Deprecated
 public class FeatureUnavailableException extends BrokerException {
     /**
      * 
