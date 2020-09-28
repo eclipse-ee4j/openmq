723e14e859ee637e0d2b7c17e1e4b84ab01cc1d9
Piotrek Zygielo <piotr@zygielo.pl>
2020-09-28 19:30:12 +0200

    Fix debug message
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- mq/main/mq-broker/broker-core/src/main/java/com/sun/messaging/jmq/jmsserver/core/Consumer.java
+++ mq/main/mq-broker/broker-core/src/main/java/com/sun/messaging/jmq/jmsserver/core/Consumer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -1457,7 +1457,7 @@ public class Consumer implements ConsumerSpi, EventBroadcaster, Serializable {
                 paused = false;
             }
             if (DEBUG) {
-                logger.log(logger.DEBUG, "Pausing consumer " + this + "[" + pauseCnt + "] " + reason);
+                logger.log(logger.DEBUG, "Resuming consumer " + this + "[" + pauseCnt + "] " + reason);
             }
         }
         checkState(null);
