896a526678d52af4fc95f23a23678bd0421d2ee9
Piotrek Zygielo <piotr@zygielo.pl>
2020-09-29 20:10:09 +0200

    Deprecate unused getter, ctors class fields and then instance field to be unused as well
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- mq/main/mq-broker/broker-core/src/main/java/com/sun/messaging/jmq/jmsserver/core/Queue.java
+++ mq/main/mq-broker/broker-core/src/main/java/com/sun/messaging/jmq/jmsserver/core/Queue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -250,13 +250,12 @@ public class Queue extends Destination {
                 try {
                     num = Integer.parseInt(value);
                 } catch (Exception ex) {
-                    throw new PropertyUpdateException(PropertyUpdateException.InvalidSetting, "bad value " + value + " expected integer", ex);
+                    throw new PropertyUpdateException("bad value " + value + " expected integer", ex);
                 }
                 if (MAX_LICENSED_ACTIVE != -1 && (num == -1 || num > MAX_LICENSED_ACTIVE))
 
                 {
-                    throw new PropertyUpdateException(PropertyUpdateException.OutOfBounds,
-                            Globals.getBrokerResources().getKString(BrokerResources.E_FEATURE_UNAVAILABLE,
+                    throw new PropertyUpdateException(Globals.getBrokerResources().getKString(BrokerResources.E_FEATURE_UNAVAILABLE,
                                     Globals.getBrokerResources().getKString(BrokerResources.M_LIC_PRIMARY_CONSUMERS, String.valueOf(MAX_LICENSED_ACTIVE))));
                 }
 
@@ -265,13 +264,12 @@ public class Queue extends Destination {
                 try {
                     num = Integer.parseInt(value);
                 } catch (Exception ex) {
-                    throw new PropertyUpdateException(PropertyUpdateException.InvalidSetting, "bad value " + value + " expected integer", ex);
+                    throw new PropertyUpdateException("bad value " + value + " expected integer", ex);
                 }
                 if (MAX_LICENSED_BACKUP != -1 && (num == -1 || num > MAX_LICENSED_BACKUP))
 
                 {
-                    throw new PropertyUpdateException(PropertyUpdateException.OutOfBounds,
-                            Globals.getBrokerResources().getKString(BrokerResources.E_FEATURE_UNAVAILABLE,
+                    throw new PropertyUpdateException(Globals.getBrokerResources().getKString(BrokerResources.E_FEATURE_UNAVAILABLE,
                                     Globals.getBrokerResources().getKString(BrokerResources.M_LIC_FAILOVER_CONSUMERS, String.valueOf(MAX_LICENSED_BACKUP))));
                 }
 
