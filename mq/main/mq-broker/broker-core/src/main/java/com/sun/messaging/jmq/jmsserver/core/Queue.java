38a9cc4a21308a1740999b8433513f0084e2f616
Piotrek Zygielo <piotr@zygielo.pl>
2020-10-01 13:49:03 +0200

    Remove unused parameter
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- mq/main/mq-broker/broker-core/src/main/java/com/sun/messaging/jmq/jmsserver/core/Queue.java
+++ mq/main/mq-broker/broker-core/src/main/java/com/sun/messaging/jmq/jmsserver/core/Queue.java
@@ -862,7 +862,7 @@ public class Queue extends Destination {
             return;
         }
         synchronized (this) {
-            makeInactive(c.consumer);
+            makeInactive();
         }
 
         if (c.local && !getIsLocal()) {
@@ -909,7 +909,7 @@ public class Queue extends Destination {
         }
     }
 
-    private void makeInactive(Consumer s) {
+    private void makeInactive() {
     }
 
     private void consumerListChanged() throws BrokerException {
