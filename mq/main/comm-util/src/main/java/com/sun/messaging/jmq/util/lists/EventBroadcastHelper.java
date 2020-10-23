aa1be82466e1acb8e295942a17942b890cfe6c25
Jonathan Coustick <jonathan.coustick@payara.fish>
2020-10-23 16:02:26 +0100

    Undone replacement of c in copyright header
    Signed-off-by: Jonathan Coustick <jonathan.coustick@payara.fish>

--- mq/main/comm-util/src/main/java/com/sun/messaging/jmq/util/lists/EventBroadcastHelper.java
+++ mq/main/comm-util/src/main/java/com/sun/messaging/jmq/util/lists/EventBroadcastHelper.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (collectionArray) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
- * Copyright (collectionArray) 2020 Payara Services Ltd.
+ * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020 Payara Services Ltd.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
