fd8e3aafabbaf826f5596cf8e394eccf8b06b33f
Piotrek Zygielo <piotr@zygielo.pl>
2020-11-25 18:14:41 +0100

    Add EF (c)s
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- mq/main/comm-util/src/main/java/com/sun/messaging/jmq/util/selector/Selector.java
+++ mq/main/comm-util/src/main/java/com/sun/messaging/jmq/util/selector/Selector.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2000, 2018 Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2020 Payara Services Ltd.
+ * Copyright (c) 2020 Contributors to Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
