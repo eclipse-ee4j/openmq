f1722946222af47a173917a6cf7dcba836373f7d
Piotrek Zygielo <piotr@zygielo.pl>
2020-12-10 15:20:53 +0100

    Drop license check in ClusterServiceListener.initSSLListener

--- mq/main/mq-broker/cluster/src/main/java/com/sun/messaging/jmq/jmsserver/multibroker/fullyconnected/ClusterImpl.java
+++ mq/main/mq-broker/cluster/src/main/java/com/sun/messaging/jmq/jmsserver/multibroker/fullyconnected/ClusterImpl.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -2236,17 +2237,6 @@ class ClusterServiceListener extends Thread {
 
         ServerSocketFactory sslfactory = null;
         try {
-            /*
-             * This can be called as a result of cluster property update. Hence does not do System.exit here. Cluster.start()
-             * Exception will cause System.exit,
-             */
-            LicenseBase license = Globals.getCurrentLicense(null);
-            if (!license.getBooleanProperty(license.PROP_ENABLE_SSL, false)) {
-                logger.log(Logger.ERROR, br.E_FATAL_FEATURE_UNAVAILABLE, br.getString(br.M_SSL_BROKER_CLUSTERS));
-                throw new BrokerException(br.getKString(br.E_FATAL_FEATURE_UNAVAILABLE, br.getString(br.M_SSL_BROKER_CLUSTERS)));
-
-            }
-
             Class TLSProtocolClass = Class.forName("com.sun.messaging.jmq.jmsserver.net.tls.TLSProtocol");
 
             if (ClusterImpl.DEBUG) {
