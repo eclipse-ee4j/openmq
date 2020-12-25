069c64c6afa7a58e2a033661f755bf0f30988402
Piotrek Zygielo <piotr@zygielo.pl>
2020-12-25 19:43:16 +0100

    Deprecated ProtocolHandler.getLicense

--- mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/ProtocolHandler.java
+++ mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/ProtocolHandler.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -2980,7 +2981,10 @@ public class ProtocolHandler {
      * Get License information from the broker
      *
      * The license data is returned in the Hashtable
+     *
+     * @deprecated since 6.1, will be removed in future release
      */
+    @Deprecated
     public Hashtable getLicense() throws JMSException {
         Hashtable licenseProps = null;
         ReadWritePacket pkt = new ReadWritePacket();
