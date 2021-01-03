3fdef2c55a5727d9f5b868295155d1d709c808a9
Piotrek Zygielo <piotr@zygielo.pl>
2021-01-03 10:32:58 +0100

    Fix PMD:UnnecessaryModifier violations

--- mq/main/persist/disk-io/src/main/java/com/sun/messaging/jmq/io/disk/ObjectInputStreamCallback.java
+++ mq/main/persist/disk-io/src/main/java/com/sun/messaging/jmq/io/disk/ObjectInputStreamCallback.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -25,5 +26,5 @@ import java.io.ByteArrayInputStream;
 
 public interface ObjectInputStreamCallback {
 
-    public ObjectInputStream getObjectInputStream(ByteArrayInputStream bis) throws IOException;
+    ObjectInputStream getObjectInputStream(ByteArrayInputStream bis) throws IOException;
 }
