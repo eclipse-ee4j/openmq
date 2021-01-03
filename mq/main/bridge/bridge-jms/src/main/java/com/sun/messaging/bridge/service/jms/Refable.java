4de996c54b78aff0b9fffea87a9190205b3c6593
Piotrek Zygielo <piotr@zygielo.pl>
2021-01-03 10:32:55 +0100

    Fix PMD:UnnecessaryModifier violations

--- mq/main/bridge/bridge-jms/src/main/java/com/sun/messaging/bridge/service/jms/Refable.java
+++ mq/main/bridge/bridge-jms/src/main/java/com/sun/messaging/bridge/service/jms/Refable.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -22,11 +23,11 @@ package com.sun.messaging.bridge.service.jms;
  */
 public interface Refable {
 
-    public String getRef();
+    String getRef();
 
-    public Object getRefed();
+    Object getRefed();
 
-    public boolean isEmbeded();
+    boolean isEmbeded();
 
-    public boolean isMultiRM();
+    boolean isMultiRM();
 }
