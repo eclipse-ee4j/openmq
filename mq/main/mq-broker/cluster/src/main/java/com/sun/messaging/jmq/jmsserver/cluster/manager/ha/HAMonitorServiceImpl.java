9cd28cb51b2d3aeed060dccdd30f52a96b2a1ce7
Piotrek Zygielo <piotr@zygielo.pl>
2021-01-03 23:13:33 +0100

    Fix ecj:unusedLocal in mq-broker/cluster

--- mq/main/mq-broker/cluster/src/main/java/com/sun/messaging/jmq/jmsserver/cluster/manager/ha/HAMonitorServiceImpl.java
+++ mq/main/mq-broker/cluster/src/main/java/com/sun/messaging/jmq/jmsserver/cluster/manager/ha/HAMonitorServiceImpl.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -969,7 +970,6 @@ public class HAMonitorServiceImpl implements HAMonitorService, ClusterListener {
                             List openTxns = new ArrayList();
                             Iterator titr = txn.iterator();
                             while (titr.hasNext()) {
-                                boolean rollbackTxn = false;
                                 TransactionUID tid = (TransactionUID) titr.next();
                                 TransactionState ts = pstore.getTransactionState(tid);
                                 if (ts == null) {
