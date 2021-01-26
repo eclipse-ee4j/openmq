c24dae3373938ed2efd885a8a138a3db9eff7510
Piotrek Zygielo <piotr@zygielo.pl>
2021-01-26 12:44:56 +0100

    Cosmetics of headers
    Remove extra line from header.
    Restore space in header line.

--- mq/main/mq-broker/broker-core/src/main/java/com/sun/messaging/jmq/jmsserver/Broker.java
+++ mq/main/mq-broker/broker-core/src/main/java/com/sun/messaging/jmq/jmsserver/Broker.java
@@ -1,7 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2020, 2021 Contributors to Eclipse Foundation. All rights reserved.
- * Copyright (c) 2020 Contributors to Eclipse Foundation. All rights reserved.
  * Copyright (c) 2020 Payara Services Ltd.
  *
  * This program and the accompanying materials are made available under the
