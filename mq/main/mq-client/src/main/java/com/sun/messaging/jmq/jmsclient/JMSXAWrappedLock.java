0abecb054204250da3906a5d85d143d73fac7fcf
Piotrek Zygielo <piotr@zygielo.pl>
2021-02-06 10:34:38 +0100

    Avoid unnecessary constructors (client)

--- mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/JMSXAWrappedLock.java
+++ mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/JMSXAWrappedLock.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -28,9 +29,6 @@ public class JMSXAWrappedLock {
 
     private Thread owner = null;
     private boolean locked = false;
-
-    public JMSXAWrappedLock() {
-    }
 
     public synchronized void acquireLock() {
         Thread me = Thread.currentThread();
--- mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/MessageConvert.java
+++ mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/MessageConvert.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -37,9 +38,6 @@ public class MessageConvert {
 
     public static synchronized MessageConvert getInstance() {
         return messageConvert;
-    }
-
-    public MessageConvert() {
     }
 
     /**
