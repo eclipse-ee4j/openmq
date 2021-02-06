9ceb3ec7657e4a3260eebf37c072da4b6d22ed8a
Piotrek Zygielo <piotr@zygielo.pl>
2021-02-06 10:33:20 +0100

    Avoid unnecessary constructors (ums)

--- mq/main/mq-ums/src/main/java/com/sun/messaging/ums/readonly/ReadOnlyResponseMessage.java
+++ mq/main/mq-ums/src/main/java/com/sun/messaging/ums/readonly/ReadOnlyResponseMessage.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -31,9 +32,6 @@ public class ReadOnlyResponseMessage {
     private Map map = null;
 
     private String responseMessage = null;
-
-    public ReadOnlyResponseMessage() {
-    }
 
     public void setResponseMap(Map map) {
         this.map = map;
