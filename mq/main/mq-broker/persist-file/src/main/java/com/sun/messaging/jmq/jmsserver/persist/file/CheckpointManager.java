ac155b546fca1d01c68636cff5c9c4d30c3c4050
Piotrek Zygielo <piotr@zygielo.pl>
2021-02-06 14:31:44 +0100

    Use Diamond Operator (mq-broker/persist-file)

--- mq/main/mq-broker/persist-file/src/main/java/com/sun/messaging/jmq/jmsserver/persist/file/CheckpointManager.java
+++ mq/main/mq-broker/persist-file/src/main/java/com/sun/messaging/jmq/jmsserver/persist/file/CheckpointManager.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -26,7 +27,7 @@ import com.sun.messaging.jmq.util.log.Logger;
 public class CheckpointManager implements Runnable {
 
     TransactionLogManager transactionLogManager;
-    private BlockingQueue<Checkpoint> checkpointQueue = new ArrayBlockingQueue<Checkpoint>(5);
+    private BlockingQueue<Checkpoint> checkpointQueue = new ArrayBlockingQueue<>(5);
     int numCheckpoints;
     private Thread runner;
     public static final Logger logger = Globals.getLogger();
--- mq/main/mq-broker/persist-file/src/main/java/com/sun/messaging/jmq/jmsserver/persist/file/LoggedMessageHelper.java
+++ mq/main/mq-broker/persist-file/src/main/java/com/sun/messaging/jmq/jmsserver/persist/file/LoggedMessageHelper.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -47,7 +48,7 @@ public class LoggedMessageHelper {
 
     public static final Logger logger = Globals.getLogger();
 
-    Map<SysMessageID, SysMessageID> loggedSendsSinceLastCheckpoint = new ConcurrentHashMap<SysMessageID, SysMessageID>();
+    Map<SysMessageID, SysMessageID> loggedSendsSinceLastCheckpoint = new ConcurrentHashMap<>();
 
     Set<SysMessageID> pendingRemove = Collections.synchronizedSet(new HashSet<SysMessageID>());
 
