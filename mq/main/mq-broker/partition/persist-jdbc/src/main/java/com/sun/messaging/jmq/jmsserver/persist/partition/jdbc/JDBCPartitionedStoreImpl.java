7a6ef227d57de9b6186dc715c480c0c7058df630
Piotrek Zygielo <piotr@zygielo.pl>
2021-02-06 10:40:00 +0100

    Avoid unnecessary constructors (broker/partition)

--- mq/main/mq-broker/partition/persist-jdbc/src/main/java/com/sun/messaging/jmq/jmsserver/persist/partition/jdbc/JDBCPartitionedStoreImpl.java
+++ mq/main/mq-broker/partition/persist-jdbc/src/main/java/com/sun/messaging/jmq/jmsserver/persist/partition/jdbc/JDBCPartitionedStoreImpl.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -47,12 +48,6 @@ import org.glassfish.hk2.api.PerLookup;
 @Service(name = "com.sun.messaging.jmq.jmsserver.persist.partition.jdbc.JDBCPartitionedStoreImpl")
 @PerLookup
 public class JDBCPartitionedStoreImpl extends AbstractPartitionedStore {
-
-    /**
-     */
-    public JDBCPartitionedStoreImpl() {
-        super();
-    }
 
     /**
      * Store a message, which is uniquely identified by it's SysMessageID, and it's list of interests and their states.
