73bebea8e2dff05b3c8a42fd794335420beb2724
Piotrek Zygielo <piotr@zygielo.pl>
2021-02-06 10:39:18 +0100

    Avoid unnecessary constructors (http-tunnel)

--- mq/main/http-tunnel/tunnel/src/main/java/com/sun/messaging/jmq/httptunnel/tunnel/client/HttpTunnelPush.java
+++ mq/main/http-tunnel/tunnel/src/main/java/com/sun/messaging/jmq/httptunnel/tunnel/client/HttpTunnelPush.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -43,9 +44,6 @@ public class HttpTunnelPush extends Thread implements HttpTunnelDefaults {
     private Vector q = null;
     private boolean stopThread = false;
     private boolean shutdownComplete = false;
-
-    public HttpTunnelPush() {
-    }
 
     /**
      * Set the default push URL and start the push thread.
--- mq/main/http-tunnel/tunnel/src/main/java/com/sun/messaging/jmq/httptunnel/tunnel/server/HttpsTunnelServerDriverImpl.java
+++ mq/main/http-tunnel/tunnel/src/main/java/com/sun/messaging/jmq/httptunnel/tunnel/server/HttpsTunnelServerDriverImpl.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -47,9 +48,6 @@ public class HttpsTunnelServerDriverImpl extends HttpTunnelServerDriverImpl impl
     private static Logger logger = Logger.getLogger("Http Tunneling");
     protected boolean trustServlet = true;
     private boolean poodleFixEnabled = true;
-
-    public HttpsTunnelServerDriverImpl() {
-    }
 
     @Override
     public void init(String serviceName, boolean trust, boolean poodleFixEnabled) throws IOException {
