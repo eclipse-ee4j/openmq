b18c8728d7f603ba78ee389970cdf82cecc75d90
Piotrek Zygielo <piotr@zygielo.pl>
2021-02-06 10:39:46 +0100

    Avoid unnecessary constructors (broker/cluster)

--- mq/main/mq-broker/cluster/src/main/java/com/sun/messaging/jmq/jmsserver/multibroker/BrokerInfo.java
+++ mq/main/mq-broker/cluster/src/main/java/com/sun/messaging/jmq/jmsserver/multibroker/BrokerInfo.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2020 Payara Services Ltd.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -45,9 +46,6 @@ public class BrokerInfo implements Serializable {
     private Integer clusterProtocolVersion = null;
 
     private transient String realRemote = null;
-
-    public BrokerInfo() {
-    }
 
     public Integer getClusterProtocolVersion() {
         return clusterProtocolVersion;
