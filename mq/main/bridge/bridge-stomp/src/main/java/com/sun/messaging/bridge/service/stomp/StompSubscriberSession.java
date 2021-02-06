5a1fd40f681a6d43d3632b12e2db393918f76c2c
Piotrek Zygielo <piotr@zygielo.pl>
2021-02-06 14:31:41 +0100

    Use Diamond Operator (bridge/bridge-stomp)

--- mq/main/bridge/bridge-stomp/src/main/java/com/sun/messaging/bridge/service/stomp/StompSubscriberSession.java
+++ mq/main/bridge/bridge-stomp/src/main/java/com/sun/messaging/bridge/service/stomp/StompSubscriberSession.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -46,7 +47,7 @@ public class StompSubscriberSession implements StompSession, StompSubscriber, Me
     private MessageConsumer _subscriber = null;
 
     private StompOutputHandler _out = null;
-    private ArrayList<Message> _unacked = new ArrayList<Message>();
+    private ArrayList<Message> _unacked = new ArrayList<>();
     private String _duraName = null;
     private StompConnectionImpl stompconn = null;
     private StompBridgeResources _sbr = null;
