1123c730f670c0c1b5e2f9be4c5b33459fdeb790
Piotrek Zygielo <piotr@zygielo.pl>
2021-06-02 21:30:31 +0200

    Add missing brace

--- mq/src/share/java/examples/ums/csharp/SendSOAPMsg.cs
+++ mq/src/share/java/examples/ums/csharp/SendSOAPMsg.cs
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -481,3 +482,4 @@ public class SendSOAPMsg
 
         sender.sendMessage();
     }
+}
