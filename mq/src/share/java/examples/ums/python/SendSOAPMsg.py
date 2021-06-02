2b1f36abec627e98ddc0c1514892ab37a585b393
Piotrek Zygielo <piotr@zygielo.pl>
2021-06-02 21:42:05 +0200

    Remove unused assignments

--- mq/src/share/java/examples/ums/python/SendSOAPMsg.py
+++ mq/src/share/java/examples/ums/python/SendSOAPMsg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #
 # Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2021 Contributors to the Eclipse Foundation
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -265,7 +266,7 @@ def main():
 
       conn = httplib.HTTPConnection(host)
       url = DEFAULT_CONTEXT_ROOT + "/simple?service=close&sid=" + sid
-      (rtnCode, respMsg) = doPost(conn, url, "")
+      doPost(conn, url, "")
       conn.close()
 
 if __name__ == '__main__':
