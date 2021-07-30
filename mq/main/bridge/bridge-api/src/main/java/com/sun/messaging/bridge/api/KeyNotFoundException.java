932680f448086b1027e8016743df7b20017efe25
Piotrek Zygielo <piotr@zygielo.pl>
2021-07-30 11:34:34 +0200

    Remove unused constructors

--- mq/main/bridge/bridge-api/src/main/java/com/sun/messaging/bridge/api/KeyNotFoundException.java
+++ mq/main/bridge/bridge-api/src/main/java/com/sun/messaging/bridge/api/KeyNotFoundException.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -36,14 +37,5 @@ public class KeyNotFoundException extends Exception {
      */
     public KeyNotFoundException(String msg) {
         super(msg);
-    }
-
-    /**
-     * create an exception with a message but no root cause
-     *
-     * @param msg the detail message
-     */
-    public KeyNotFoundException(String msg, Throwable thr) {
-        super(msg, thr);
     }
 }
