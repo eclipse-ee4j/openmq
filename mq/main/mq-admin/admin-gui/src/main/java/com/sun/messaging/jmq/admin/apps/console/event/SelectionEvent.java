38446475ca492a9e4c828074c05761bc0cae039a
Piotrek Zygielo <piotr@zygielo.pl>
2021-09-03 13:27:50 +0200

    Deprecate unused constructor

--- mq/main/mq-admin/admin-gui/src/main/java/com/sun/messaging/jmq/admin/apps/console/event/SelectionEvent.java
+++ mq/main/mq-admin/admin-gui/src/main/java/com/sun/messaging/jmq/admin/apps/console/event/SelectionEvent.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -44,7 +45,9 @@ public class SelectionEvent extends AdminEvent {
      * Creates an instance of SelectionEvent
      *
      * @param source the object where the event originated
+     * @deprecated since 6.3, will be removed without replacement
      */
+    @Deprecated
     public SelectionEvent(Object source) {
         super(source);
     }
