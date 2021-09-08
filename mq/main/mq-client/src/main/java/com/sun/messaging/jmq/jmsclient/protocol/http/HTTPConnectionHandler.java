70a8107971506012a964795401f647e5225d5ed3
Piotrek Zygielo <piotr@zygielo.pl>
2021-09-08 13:27:44 +0200

    Remove unnecessary comments

--- mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/protocol/http/HTTPConnectionHandler.java
+++ mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/protocol/http/HTTPConnectionHandler.java
@@ -15,10 +15,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-/*
- * @(#)HTTPConnectionHandler.java	1.15 06/27/07
- */
-
 package com.sun.messaging.jmq.jmsclient.protocol.http;
 
 import java.io.*;
