18e752ad9447ef1e647076a9c46ee1da1f101937
Piotrek Zygielo <piotr@zygielo.pl>
2021-10-10 20:59:57 +0200

    Remove unnecessary or empty comments

--- mq/main/mq-admin/admin-gui/src/main/java/com/sun/messaging/jmq/admin/apps/console/event/ObjAdminEvent.java
+++ mq/main/mq-admin/admin-gui/src/main/java/com/sun/messaging/jmq/admin/apps/console/event/ObjAdminEvent.java
@@ -15,10 +15,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-/*
- * @(#)ObjAdminEvent.java	1.18 06/28/07
- */
-
 package com.sun.messaging.jmq.admin.apps.console.event;
 
 import java.util.Properties;
@@ -34,9 +30,6 @@ import com.sun.messaging.jmq.admin.objstore.ObjStoreAttrs;
  * The fields of this event include the various pieces of information needed for object management tasks.
  */
 public class ObjAdminEvent extends AdminEvent {
-    /**
-     * 
-     */
     private static final long serialVersionUID = 5437958055298936323L;
     /*
      * ObjAdminEvent event types
