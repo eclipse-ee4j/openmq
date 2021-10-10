86c319606d85792b0ee462eb4db71a8ea1def686
Piotrek Zygielo <piotr@zygielo.pl>
2021-10-10 10:24:46 +0200

    Remove unnecessary or empty comments

--- mq/main/http-tunnel/tunnel-api-share/src/main/java/com/sun/messaging/jmq/httptunnel/api/share/HttpTunnelDefaults.java
+++ mq/main/http-tunnel/tunnel-api-share/src/main/java/com/sun/messaging/jmq/httptunnel/api/share/HttpTunnelDefaults.java
@@ -15,10 +15,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-/*
- * @(#)HttpTunnelDefaults.java	1.12 06/28/07
- */
-
 package com.sun.messaging.jmq.httptunnel.api.share;
 
 /**
