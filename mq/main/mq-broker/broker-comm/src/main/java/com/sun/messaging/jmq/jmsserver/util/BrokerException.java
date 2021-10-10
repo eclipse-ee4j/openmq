c5e4c71514a1a642790c43874af5eb54cc4c0526
Piotrek Zygielo <piotr@zygielo.pl>
2021-10-10 18:03:51 +0200

    Remove unnecessary or empty comments

--- mq/main/mq-broker/broker-comm/src/main/java/com/sun/messaging/jmq/jmsserver/util/BrokerException.java
+++ mq/main/mq-broker/broker-comm/src/main/java/com/sun/messaging/jmq/jmsserver/util/BrokerException.java
@@ -15,10 +15,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-/*
- * @(#)BrokerException.java	1.13 06/29/07
- */
-
 package com.sun.messaging.jmq.jmsserver.util;
 
 import com.sun.messaging.jmq.io.Status;
@@ -28,9 +24,6 @@ import com.sun.messaging.jmq.io.Status;
  */
 
 public class BrokerException extends Exception {
-    /**
-     * 
-     */
     private static final long serialVersionUID = -7016435578580516234L;
 
     /**
