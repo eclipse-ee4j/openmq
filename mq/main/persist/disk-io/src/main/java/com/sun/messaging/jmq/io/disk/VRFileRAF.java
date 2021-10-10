df9ac9363dc3f2ccb2924ec9a638622c3da1c9e7
Piotrek Zygielo <piotr@zygielo.pl>
2021-10-10 18:06:10 +0200

    Remove unnecessary or empty comments

--- mq/main/persist/disk-io/src/main/java/com/sun/messaging/jmq/io/disk/VRFileRAF.java
+++ mq/main/persist/disk-io/src/main/java/com/sun/messaging/jmq/io/disk/VRFileRAF.java
@@ -15,10 +15,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-/*
- * @(#)VRFileRAF.java	1.11 08/28/07
- */
-
 package com.sun.messaging.jmq.io.disk;
 
 import java.io.*;
