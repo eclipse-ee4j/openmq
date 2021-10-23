789f5a78895e873c9d1d79ea66c3fb335e507505
Piotrek Zygielo <piotr@zygielo.pl>
2021-10-23 15:47:17 +0200

    Remove unnecessary or empty comments

--- mq/main/mq-share/src/main/java/com/sun/messaging/jmq/auth/jaas/MQConnectionPermission.java
+++ mq/main/mq-share/src/main/java/com/sun/messaging/jmq/auth/jaas/MQConnectionPermission.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -14,10 +15,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-/*
- * @(#)MQConnectionPermission.java	1.4 06/27/07
- */
-
 package com.sun.messaging.jmq.auth.jaas;
 
 /**
@@ -29,9 +26,6 @@ public final class MQConnectionPermission extends MQBasicPermission {
 
     private static final long serialVersionUID = 1919911567794615301L;
 
-    /**
-     *
-     */
     public MQConnectionPermission(String name) {
         super(name);
     }
