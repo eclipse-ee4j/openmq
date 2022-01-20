c5d8eb7c873adeac1ed2649796eedd7a87f4e50c
Piotrek Zygielo <piotr@zygielo.pl>
2022-01-20 09:25:18 +0100

    Extract top-level class to separate file (mq-broker/broker-comm)

--- mq/main/mq-broker/broker-comm/src/main/java/com/sun/messaging/jmq/jmsserver/config/UpdateProperties.java
+++ mq/main/mq-broker/broker-comm/src/main/java/com/sun/messaging/jmq/jmsserver/config/UpdateProperties.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2000, 2020 Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2020 Payara Services Ltd.
- * Copyright (c) 2021 Contributors to the Eclipse Foundation
+ * Copyright (c) 2021, 2022 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -771,108 +771,3 @@ public class UpdateProperties extends Properties {
 
 }
 
-/**
- * This class is used by updateProperties in place of the normal value for a property to maintain the list of listeners
- * for that property.
- * <P>
- *
- * This class maintains both the value and the set of listeners
- */
-class WatchedProperty {
-    /**
-     * value of the property
-     */
-    private String value;
-
-    /**
-     * set of listeners for the property
-     */
-    private Vector listeners = null;
-
-    /**
-     * create a new watched property object with a null value
-     */
-    WatchedProperty() {
-        this(null);
-    }
-
-    /**
-     * create a new watched property object
-     *
-     * @param value value of the property
-     */
-    WatchedProperty(String value) {
-        this.value = value;
-        listeners = new Vector();
-    }
-
-    /**
-     * sets the value of the property object
-     *
-     * @param value the new value of the property
-     */
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    /**
-     * returns the value of the property object
-     *
-     * @return the current value of the property
-     */
-    public String getValue() {
-        return value;
-    }
-
-    /**
-     * adds a new config listener
-     *
-     * @param listener the listener object to add
-     */
-    public void addListener(ConfigListener listener) {
-        listeners.addElement(listener);
-    }
-
-    /**
-     * removes an existing config listener
-     *
-     * @param listener the listener object to remove as a listener
-     */
-    public void removeListener(ConfigListener listener) {
-        listeners.removeElement(listener);
-    }
-
-    /**
-     * clears out all config listener
-     *
-     */
-    void clearListeners() {
-        listeners = new Vector();
-    }
-
-    /**
-     * returns the vector of all config listener
-     *
-     * @return the vector of listeners (if there are no listeners, it will return an empty vectory NOT a null vector)
-     */
-    Vector getListeners() {
-        return this.listeners;
-    }
-
-    /**
-     * prints out the string for a watched Property
-     */
-    @Override
-    public String toString() {
-        StringBuilder buf = new StringBuilder();
-        buf.append("WatchedProperty(").append(listeners.size()).append(") value = [\"").append(value).append("\"] {");
-        for (int i = 0; i < listeners.size(); i++) {
-            if (i > 0) {
-                buf.append(", ");
-            }
-            buf.append(listeners.elementAt(i).getClass());
-        }
-        buf.append('}');
-        return buf.toString();
-    }
-}
