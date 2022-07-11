ebf9d3d2980cd36162a6b98269a8bd3572280795
Piotrek Zygielo <piotr.zygielo@gmail.com>
2022-07-11 21:44:03 +0200

    Update (c)s

--- mq/main/comm-util/src/main/java/com/sun/messaging/jmq/util/lists/NFLPriorityFifoSet.java
+++ mq/main/comm-util/src/main/java/com/sun/messaging/jmq/util/lists/NFLPriorityFifoSet.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2020 Payara Services Ltd.
- * Copyright (c) 2020, 2021 Contributors to Eclipse Foundation
+ * Copyright (c) 2020, 2022 Contributors to Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
