0bf3c03a5d1b062c7957b82bad2b1f2f2d54cfb0
Piotrek Zygielo <piotr.zygielo@gmail.com>
2022-09-10 23:16:15 +0200

    Extract help files to new module

---
+++ mq/main/helpfiles/src/main/resources/Master.hs
@@ -0,0 +1,34 @@
+<?xml version='1.0' encoding='ISO-8859-1'?>
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+                         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+<!--
+
+    Copyright (c) 2000-2017 Oracle and/or its affiliates. All rights reserved.
+
+    This program and the accompanying materials are made available under the
+    terms of the Eclipse Public License v. 2.0, which is available at
+    http://www.eclipse.org/legal/epl-2.0.
+
+    This Source Code may also be made available under the following Secondary
+    Licenses when the conditions for such availability set forth in the
+    Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+    version 2 with the GNU Classpath Exception, which is available at
+    https://www.gnu.org/software/classpath/license.html.
+
+    SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+
+-->
+
+<helpset version="1.0" xml:lang="en">
+	<title>Oracle GlassFish(tm) Server Message Queue Administration Console Help</title>
+	<maps>
+		<homeID>overview</homeID>
+		<mapref location="en/Master.jhm"/>
+	</maps>
+	<view>
+		<name>TOC</name>
+		<label>Table of Contents</label>
+		<type>javax.help.TOCView</type>
+		<data>en/MasterTOC.xml</data>
+	</view>
+</helpset>
