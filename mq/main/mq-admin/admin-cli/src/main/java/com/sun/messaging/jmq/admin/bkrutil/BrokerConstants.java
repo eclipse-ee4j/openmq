da7d2da9df162976c80ed3c82e7436d6ac9e72c6
Piotrek Zygielo <piotr.zygielo@gmail.com>
2022-09-12 22:52:50 +0200

    Remove unnecessary comments

--- mq/main/mq-admin/admin-cli/src/main/java/com/sun/messaging/jmq/admin/bkrutil/BrokerConstants.java
+++ mq/main/mq-admin/admin-cli/src/main/java/com/sun/messaging/jmq/admin/bkrutil/BrokerConstants.java
@@ -15,10 +15,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-/*
- * @(#)BrokerConstants.java	1.32 07/11/07
- */
-
 package com.sun.messaging.jmq.admin.bkrutil;
 
 import java.util.List;
