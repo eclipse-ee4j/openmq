359c749e040f9a0a523e8f37ab213cc673ad6d84
Piotrek Zygielo <piotr.zygielo@gmail.com>
2023-02-25 08:26:55 +0100

    Bump minor version

--- mq/src/share/cclient/cshim/mqversion.h
+++ mq/src/share/cclient/cshim/mqversion.h
@@ -25,9 +25,9 @@
 
 #define MQ_NAME    "Eclipse OpenMQ(tm)"
 
-#define MQ_VERSION "6.4.0"
+#define MQ_VERSION "6.5.0"
 #define MQ_VMAJOR   6
-#define MQ_VMINOR   4
+#define MQ_VMINOR   5
 #define MQ_VMICRO   0
 #define MQ_SVCPACK  0
 #define MQ_URELEASE 0
