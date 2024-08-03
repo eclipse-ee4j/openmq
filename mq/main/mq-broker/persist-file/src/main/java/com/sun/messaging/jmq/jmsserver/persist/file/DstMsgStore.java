e4d489cd839432b0857112546f45e3aaa2a697e0
Piotrek Zygielo <piotr.zygielo@gmail.com>
2024-08-03 14:55:25 +0200

    Apply rewrite:UpgradeToJava17 patches

--- mq/main/mq-broker/persist-file/src/main/java/com/sun/messaging/jmq/jmsserver/persist/file/DstMsgStore.java
+++ mq/main/mq-broker/persist-file/src/main/java/com/sun/messaging/jmq/jmsserver/persist/file/DstMsgStore.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2021 Contributors to the Eclipse Foundation
+ * Copyright (c) 2021, 2024 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -613,9 +613,9 @@ class DstMsgStore extends RandomAccessStore {
             if (objToReturn != null) {
                 Object result = null;
                 
-                if (objToReturn instanceof SysMessageID) {
+                if (objToReturn instanceof SysMessageID iD) {
                     try {
-                        result = parent.getMessage((SysMessageID) objToReturn);
+                        result = parent.getMessage(iD);
                     } catch (IOException | BrokerException e) {
                         // failed to load message
                         // log error; and continue
--- mq/main/mq-broker/persist-file/src/main/java/com/sun/messaging/jmq/jmsserver/persist/file/PreparedTxnStore.java
+++ mq/main/mq-broker/persist-file/src/main/java/com/sun/messaging/jmq/jmsserver/persist/file/PreparedTxnStore.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2021, 2022 Contributors to the Eclipse Foundation
+ * Copyright (c) 2021, 2024 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -649,9 +649,9 @@ class PreparedTxnStore extends RandomAccessStore {
             if (objToReturn != null) {
                 Object result = null;
                 
-                if (objToReturn instanceof TransactionUID) {
+                if (objToReturn instanceof TransactionUID iD) {
                     try {
-                        result = parent.getTransaction((TransactionUID) objToReturn);
+                        result = parent.getTransaction(iD);
                     } catch (IOException | BrokerException e) {
                         // failed to load message
                         // log error; and continue
