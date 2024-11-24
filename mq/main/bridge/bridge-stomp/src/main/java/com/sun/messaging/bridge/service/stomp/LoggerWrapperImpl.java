7fe17cf4485edc648dd1a9f9ca89d3a7b1a3f0fd
Piotrek Zygielo <piotr.zygielo@gmail.com>
2024-11-24 10:59:30 +0100

    Make LoggerWrapperImpl final

--- mq/main/bridge/bridge-stomp/src/main/java/com/sun/messaging/bridge/service/stomp/LoggerWrapperImpl.java
+++ mq/main/bridge/bridge-stomp/src/main/java/com/sun/messaging/bridge/service/stomp/LoggerWrapperImpl.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -23,7 +24,7 @@ import com.sun.messaging.jmq.util.LoggerWrapper;
 /**
  * @author amyk
  */
-public class LoggerWrapperImpl implements LoggerWrapper {
+public final class LoggerWrapperImpl implements LoggerWrapper {
     private final Logger logger;
 
     public LoggerWrapperImpl(Logger logger) {
