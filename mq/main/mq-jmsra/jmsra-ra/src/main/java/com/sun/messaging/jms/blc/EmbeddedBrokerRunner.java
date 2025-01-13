c4341141977de7dfb4022dcea53afc25a4bc23be
Piotrek Zygielo <piotr.zygielo@gmail.com>
2025-01-13 11:05:25 +0100

    Fix PMD:AppendCharacterWithChar

--- mq/main/mq-jmsra/jmsra-ra/src/main/java/com/sun/messaging/jms/blc/EmbeddedBrokerRunner.java
+++ mq/main/mq-jmsra/jmsra-ra/src/main/java/com/sun/messaging/jms/blc/EmbeddedBrokerRunner.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2020, 2022 Contributors to Eclipse Foundation. All rights reserved.
+ * Copyright (c) 2020, 2025 Contributors to Eclipse Foundation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -275,7 +275,7 @@ public class EmbeddedBrokerRunner implements BrokerEventListener {
             }
             builderValue.append(s);
             if (st.hasMoreTokens()){
-                builderValue.append(" ");
+                builderValue.append(' ');
             }
         }
 
