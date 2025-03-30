3250532a172b1bb300acc930aff277a454f1014a
Piotrek Zygielo <piotr.zygielo@gmail.com>
2025-03-30 19:52:12 +0200

    Fix typo to enable javadoc link

--- mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/runtime/BrokerInstance.java
+++ mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/runtime/BrokerInstance.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012, 2017 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2021, 2022 Contributors to the Eclipse Foundation
+ * Copyright (c) 2021, 2025 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -37,7 +37,7 @@ public interface BrokerInstance {
      *
      * @return The resulting Properties that represent the command line parameters passed in.
      *
-     * @throws IllegalArguementException If args contain any invalid option.
+     * @throws IllegalArgumentException If args contain any invalid option.
      */
     Properties parseArgs(String[] args);
 
