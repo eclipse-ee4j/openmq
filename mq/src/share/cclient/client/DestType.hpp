3c29487f7a6187757f91b8442c2cc14b0ac61684
Piotrek Zygielo <piotr.zygielo@gmail.com>
2025-07-09 18:58:46 +0200

    Remove unused constants

--- mq/src/share/cclient/client/DestType.hpp
+++ mq/src/share/cclient/client/DestType.hpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025 Contributors to Eclipse Foundation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -26,8 +27,5 @@
 static const PRInt32 DEST_TYPE_QUEUE         = 0x00000001;
 static const PRInt32 DEST_TYPE_TOPIC         = 0x00000002;
 static const PRInt32 DEST_TEMP               = 0x00000010;
-static const PRInt32 DEST_FLAVOR_SINGLE      = 0x00000100;
-static const PRInt32 DEST_FLAVOR_RROBIN      = 0x00000200;
-static const PRInt32 DEST_FLAVOR_FAILOVER    = 0x00000400;
 
 #endif // DESTTYPE_HPP
