302f3f913e0676988deda23e971ba49dbaa3dda8
Piotrek Zygielo <piotr.zygielo@gmail.com>
2026-03-18 23:10:11 +0100

    Drop unused `main` method

--- mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/MQAddress.java
+++ mq/main/mq-client/src/main/java/com/sun/messaging/jmq/jmsclient/MQAddress.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2021, 2024 Contributors to the Eclipse Foundation
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -96,20 +96,4 @@ public class MQAddress extends com.sun.messaging.jmq.io.MQAddress {
 
         return ret;
     }
-
-    public static void main(String args[]) throws Exception {
-        MQAddress addr = createMQAddress(args[0]);
-        System.out.println("schemeName = " + addr.getSchemeName());
-        if (addr.getIsHTTP()) {
-            System.out.println("URL = " + addr.getURL());
-        } else {
-            System.out.println("host = " + addr.getHostName());
-            System.out.println("port = " + addr.getPort());
-        }
-        System.out.println("serviceName = " + addr.getServiceName());
-        System.out.println("handlerClass = " + addr.getHandlerClass());
-        System.out.println("isFinal = " + addr.isServicePortFinal());
-        System.out.println("properties = " + addr.props);
-    }
 }
-
