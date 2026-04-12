fd6980e51c58e4ab4dc4d6101b473558ca3fc8f6
Piotrek Zygielo <piotr.zygielo@gmail.com>
2026-04-12 14:01:53 +0200

    Remove unused constructor

--- mq/main/mq-broker/mqbroker-core/src/main/java/com/sun/messaging/jmq/jmsserver/service/imq/OperationRunnableFactory.java
+++ mq/main/mq-broker/mqbroker-core/src/main/java/com/sun/messaging/jmq/jmsserver/service/imq/OperationRunnableFactory.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -23,10 +24,6 @@ public class OperationRunnableFactory implements RunnableFactory {
 
     public OperationRunnableFactory(boolean blocking) {
         this.blocking = blocking;
-    }
-
-    public OperationRunnableFactory() {
-        this(true);
     }
 
     @Override
