1384bc0dc78db75553c864cba49a8c4145f52ce5
Piotrek Zygielo <piotr.zygielo@gmail.com>
2026-09-18 21:06:55 +0200

    Use explicit class-level monitors (`:mqadmin-cli`)

--- mq/main/mq-admin/mqadmin-cli/src/main/java/com/sun/messaging/jmq/admin/objstore/ObjStoreManager.java
+++ mq/main/mq-admin/mqadmin-cli/src/main/java/com/sun/messaging/jmq/admin/objstore/ObjStoreManager.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2000, 2017 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2020, 2022 Contributors to Eclipse Foundation. All rights reserved.
+ * Copyright (c) 2020 Contributors to Eclipse Foundation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -29,6 +29,7 @@ import com.sun.messaging.jmq.admin.objstore.jndi.JNDIStore;
  */
 @SuppressWarnings("JdkObsolete")
 public class ObjStoreManager {
+    private static final Object CLASS_LOCK = new Object();
 
     // Debug messages on / off.
     private boolean DEBUG = false;
@@ -61,12 +62,14 @@ public class ObjStoreManager {
      *
      * @return ObjStoreManager the only one instance of this class
      */
-    public static synchronized ObjStoreManager getObjStoreManager() {
+    public static ObjStoreManager getObjStoreManager() {
+        synchronized (CLASS_LOCK) {
 
         if (mgr == null) {
             mgr = new ObjStoreManager();
         }
         return mgr;
+        }
     }
 
     /**
